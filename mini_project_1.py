c37b8551640c61ab7c118252de9f1d1ca0c6e236
Nirupam <joardernirupam@gmail.com>
2026-05-24 18:57:15 +0530

    completed code

--- mini_project_1.py
+++ mini_project_1.py
@@ -1,24 +1,37 @@
-name =  int(input("Enter Total Students:"))
+total_students = int(input("Enter Total Students: "))
+
 Datastudent = []
-for i in range(name):
-    print(f"Enter Details of the Students{i+1}")
-    name = input(" Name:")
-    roll_no = int(input("Roll Number:"))
-    marks = int(input("Marks:"))
+
+for i in range(total_students):
+    print(f"\nEnter Details of Student {i+1}")
+
+    name = input("Name: ")
+    roll_no = int(input("Roll Number: "))
+    marks = int(input("Marks: "))
+
     if marks > 90:
         grades = "EX"
     elif marks > 80:
         grades = "A"
     elif marks > 70:
-        grades = "B"    
+        grades = "B"
     elif marks > 60:
-        grades = "C"    
+        grades = "C"
     elif marks >= 35:
-        grades = "D"    
+        grades = "D"
     else:
         grades = "F"
+
     students = {
-        "NAME": name,
-        "ROLL_NO": roll_no,
-        "Marks": marks,
-        "Grades": grades
+        "name": name,
+        "roll_no": roll_no,
+        "marks": marks,
+        "grades": grades
+    }
+
+    Datastudent.append(students)
+
+print("\nAll Students Data")
+
+for s in Datastudent:
+    print(f"{s['name']} - Roll Number: {s['roll_no']} - Marks: {s['marks']} - Grades: {s['grades']}")
